0ac3b35c6d7ab09d557b860bdee4753a8559e0b5
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-05-10 22:28:26 +0200

    add simple js
    add simple js to test linking

---
+++ js/index.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+let num = 6;
+if(num >= 0){
+    console.log("Positive")
+}
+else{
+    console.log("Negative")
+}
+
+
+if(num%2 === 0){
+    console.log("Value is even");
+}
+else{
+    console.log("Value is odd");
+}
